0e9903c0bff613dea16d958061bcd7937c56296d
gabriel santos <gabriel89toca@gmail.com>
2024-11-25 19:58:16 -0300

    Primeiros posts com json

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 import express from "express";
 
+
+const posts = [
+    {
+      descricao: "gato fazendo brincadeiras",
+      imagem: "c:/teste/pagina/imagem1.png"
+    },
+    {
+      descricao: "Paisagem montanhosa ao amanhecer",
+      imagem: "montanha_amanhecer.jpg"
+    },
+    {
+      descricao: "Cachorro correndo na praia",
+      imagem: "cachorro_praia.png"
+    },
+    {
+      descricao: "Pratos deliciosos da culinária italiana",
+      imagem: "comida_italiana.jpg"
+    },
+    {
+      descricao: "Cidade moderna à noite",
+      imagem: "cidade_noite.jpg"
+    }
+  ];
+
 const app = express();
 
 app.listen(3000, () => {
+    app.use(express.json());
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas á imersão alura backend..");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
