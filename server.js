5969f58039292b09fe751902faf29de76c815e94
gabriel santos <gabriel89toca@gmail.com>
2024-11-25 20:38:53 -0300

    Gerando rotas com ID

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 import express from "express";
 
 
+let nextId = 1;
+
 const posts = [
-    {
-      descricao: "gato fazendo brincadeiras",
-      imagem: "c:/teste/pagina/imagem1.png"
-    },
-    {
-      descricao: "Paisagem montanhosa ao amanhecer",
-      imagem: "montanha_amanhecer.jpg"
-    },
-    {
-      descricao: "Cachorro correndo na praia",
-      imagem: "cachorro_praia.png"
-    },
-    {
-      descricao: "Pratos deliciosos da culinária italiana",
-      imagem: "comida_italiana.jpg"
-    },
-    {
-      descricao: "Cidade moderna à noite",
-      imagem: "cidade_noite.jpg"
-    }
-  ];
+  {
+    id: nextId++,
+    descricao: "gato fazendo brincadeiras",
+    imagem: "c:/teste/pagina/imagem1.png"
+  },
+  {
+    id: nextId++,
+    descricao: "Paisagem montanhosa ao amanhecer",
+    imagem: "montanha_amanhecer.jpg"
+  },
+  {
+    id: nextId++,
+    descricao: "Cachorro correndo na praia",
+    imagem: "cachorro_praia.png"
+  },
+  {
+    id: nextId++,
+    descricao: "Pratos deliciosos da culinária italiana",
+    imagem: "comida_italiana.jpg"
+  },
+  {
+    id: nextId++,
+    descricao: "Cidade moderna à noite",
+    imagem: "cidade_noite.jpg"
+  }
+];
 
 const app = express();
 
@@ -33,4 +40,16 @@ app.listen(3000, () => {
 
 app.get("/posts", (req, res) => {
     res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
